02a539ffbdb151994245db70ecc95972ce4f9c63
hloi <hloi@qcc.mass.edu>
2022-11-04 14:46:13 -0400

    Initial commit

---
+++ Phone_Directory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hloi on 11/4/2022.
+//
+
+#include "Phone_Directory.h"
---
+++ Phone_Directory.h
@@ -0,0 +1,34 @@
+//
+// Created by hloi on 11/4/2022.
+//
+
+#ifndef CH09MAPEXAMPLE_PHONE_DIRECTORY_H
+#define CH09MAPEXAMPLE_PHONE_DIRECTORY_H
+
+#include <string>
+#include <map>
+
+class Phone_Directory {
+public:
+    Phone_Directory();
+    ~Phone_Directory();
+    void load_data(const std::string source_name);
+    std::string lookup_entry(const::string& name) const;
+    std::string add_or_change_entry(const std::string& name,
+                                    const std::string& number);
+    std::string remove_entry(const std::string & name);
+    void save();
+
+private:
+    typedef std::map< std::string, std::string >::iterator iterator;
+    typedef std::map< std::string, std::string >::const_iterator const_iterator;
+
+    std::map<std::string, std::string> the_directory;
+    std::string source_name;
+    bool modified;
+
+
+};
+
+
+#endif //CH09MAPEXAMPLE_PHONE_DIRECTORY_H
